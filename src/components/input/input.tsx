c5cea53bb2bebeb0a2ef9733fbc557cd699e3cd2
HenriqueOmena <88lmao@gmail.com>
2020-12-28 23:35:08 +0000

    Install and aplly react hook form on register form

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -5,13 +5,18 @@ import { Container } from './input.style';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon?: React.ComponentType<IconBaseProps>;
+  inputRef?: React.ClassAttributes<HTMLInputElement>['ref'];
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...propsRest }) => {
+const Input: React.FC<InputProps> = ({
+  inputRef,
+  icon: Icon,
+  ...propsRest
+}) => {
   return (
     <Container>
       {Icon && <Icon size={20} />}
-      <input {...propsRest} />
+      <input ref={inputRef} {...propsRest} />
     </Container>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,30 +3,49 @@ import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import logoImg from 'assets/logo.svg';
 import Input from 'components/input';
 import Button from 'components/button';
-import Title from 'components/Title/Title';
-import SubTitle from 'components/SubTitle/SubTitle';
+import { useForm } from 'react-hook-form';
 import { BackgroundImage, Container, Content } from './styles';
 
+type Inputs = {
+  email: string;
+  name: string;
+  password: string;
+};
+
 const SignUp: React.FC = () => {
+  const { register, handleSubmit, errors } = useForm<Inputs>();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <Container>
       <BackgroundImage />
       <Content>
         <img src={logoImg} alt="BarberAppointment" />
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h1>Faça seu Cadastro</h1>
 
-          <Input icon={FiMail} name="email" placeholder="Email" />
-          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input
+            icon={FiMail}
+            name="email"
+            placeholder="Email"
+            inputRef={register}
+          />
+          <Input
+            icon={FiUser}
+            name="name"
+            placeholder="Nome"
+            inputRef={register}
+          />
           <Input
             icon={FiLock}
             name="password"
             type="password"
             placeholder="Senha"
+            inputRef={register}
           />
 
-          <Button> Cadastrar </Button>
+          <Button type="submit"> Cadastrar </Button>
         </form>
 
         <a href="#create">
